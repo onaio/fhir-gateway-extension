335f87364549571867a291e266808948e4f50536
lincmba <lincolncmba@gmail.com>
2024-03-20 18:17:32 +0300

    Address requested changes.

--- plugins/src/main/java/org/smartregister/fhir/gateway/plugins/SyncAccessDecision.java
+++ plugins/src/main/java/org/smartregister/fhir/gateway/plugins/SyncAccessDecision.java
@@ -4,7 +4,13 @@ import static org.smartregister.fhir.gateway.plugins.EnvUtil.getEnvironmentVar;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
