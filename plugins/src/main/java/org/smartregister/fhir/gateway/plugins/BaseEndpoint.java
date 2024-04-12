66413ef1394e0b9d9c75c161ea8d7f0553d1e5fb
lincmba <lincolncmba@gmail.com>
2024-04-12 16:39:35 +0300

    Refactor code, utilize BaeEndpoint class

--- plugins/src/main/java/org/smartregister/fhir/gateway/plugins/BaseEndpoint.java
+++ plugins/src/main/java/org/smartregister/fhir/gateway/plugins/BaseEndpoint.java
@@ -2,4 +2,10 @@ package org.smartregister.fhir.gateway.plugins;
 
 import javax.servlet.http.HttpServlet;
 
-public abstract class BaseEndpoint extends HttpServlet {}
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+
+public abstract class BaseEndpoint extends HttpServlet {
+    protected final FhirContext fhirR4Context = FhirContext.forR4();
+    protected final IParser fhirR4JsonParser = fhirR4Context.newJsonParser().setPrettyPrint(true);
+}
