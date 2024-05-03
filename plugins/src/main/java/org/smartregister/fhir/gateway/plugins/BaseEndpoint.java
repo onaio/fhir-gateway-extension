81952287aea8b9789fc8f722bc54c1006da54a6c
lincmba <lincolncmba@gmail.com>
2024-05-03 09:58:06 +0300

    Add cors headers to custom endpoint preflight requests

--- plugins/src/main/java/org/smartregister/fhir/gateway/plugins/BaseEndpoint.java
+++ plugins/src/main/java/org/smartregister/fhir/gateway/plugins/BaseEndpoint.java
@@ -1,6 +1,8 @@
 package org.smartregister.fhir.gateway.plugins;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
@@ -8,4 +10,9 @@ import ca.uhn.fhir.parser.IParser;
 public abstract class BaseEndpoint extends HttpServlet {
     protected final FhirContext fhirR4Context = FhirContext.forR4();
     protected final IParser fhirR4JsonParser = fhirR4Context.newJsonParser().setPrettyPrint(true);
+
+    @Override
+    protected void doOptions(HttpServletRequest request, HttpServletResponse response) {
+        RestUtils.addCorsHeaders(response);
+    }
 }
