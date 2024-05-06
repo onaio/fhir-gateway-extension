106b4f989bd40d70d5981660d1c1c4328006af1d
Martin Ndegwa <mndegwa@ona.io>
2024-05-06 10:04:52 +0300

    Move custom endpoint classes to own package

--- plugins/src/main/java/org/smartregister/fhir/gateway/plugins/endpoint/BaseEndpoint.java
+++ plugins/src/main/java/org/smartregister/fhir/gateway/plugins/endpoint/BaseEndpoint.java
@@ -6,13 +6,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
+import com.google.fhir.gateway.TokenVerifier;
+
+import java.io.IOException;
 
 public abstract class BaseEndpoint extends HttpServlet {
+    protected final TokenVerifier tokenVerifier = TokenVerifier.createFromEnvVars();
     protected final FhirContext fhirR4Context = FhirContext.forR4();
     protected final IParser fhirR4JsonParser = fhirR4Context.newJsonParser().setPrettyPrint(true);
 
     @Override
     protected void doOptions(HttpServletRequest request, HttpServletResponse response) {
         RestUtils.addCorsHeaders(response);
+    }
+    protected BaseEndpoint() throws IOException {
     }
 }
