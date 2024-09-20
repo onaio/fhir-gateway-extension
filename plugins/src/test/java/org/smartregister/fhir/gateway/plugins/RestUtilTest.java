58151cac8844120c31ddf3a7e39063a8f0edf82c
lincmba <lincolncmba@gmail.com>
2024-09-20 14:16:28 +0300

    Add tests for RestUtil

--- plugins/src/test/java/org/smartregister/fhir/gateway/plugins/RestUtilTest.java
+++ plugins/src/test/java/org/smartregister/fhir/gateway/plugins/RestUtilTest.java
@@ -5,11 +5,34 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.google.fhir.gateway.TokenVerifier;
 
 public class RestUtilTest {
+
+    @Test(expected = RuntimeException.class)
+    public void testCheckAuthenticationThrowsExceptionWhenNoAuthHeader() {
+        HttpServletRequest requestMock = mock(HttpServletRequest.class);
+        TokenVerifier tokenVerifierMock = mock(TokenVerifier.class);
+        Mockito.when(requestMock.getHeader(Constants.AUTHORIZATION)).thenReturn(null);
+        RestUtils.checkAuthentication(requestMock, tokenVerifierMock);
+    }
+
+    @Test
+    public void testCheckAuthenticationCallsTokenVerifierWhenAuthHeaderExists() {
+        HttpServletRequest requestMock = mock(HttpServletRequest.class);
+        TokenVerifier tokenVerifierMock = mock(TokenVerifier.class);
+        String authHeader = "Bearer someToken";
+        Mockito.when(requestMock.getHeader(Constants.AUTHORIZATION)).thenReturn(authHeader);
+        RestUtils.checkAuthentication(requestMock, tokenVerifierMock);
+        verify(tokenVerifierMock).decodeAndVerifyBearerToken(authHeader);
+    }
+
     @Test
     public void testAddCorsHeadersSetsCorsHeaders() {
         HttpServletResponse responseMock = mock(HttpServletResponse.class);
