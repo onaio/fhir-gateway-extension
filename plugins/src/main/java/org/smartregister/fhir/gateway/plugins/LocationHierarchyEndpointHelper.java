5bac55f2fbe253b9acfc07ce9d47d778fbf6a246
Martin Ndegwa <mndegwa@ona.io>
2024-12-16 09:02:13 +0300

    FHIR Gateway Extensions enhancements - Fix REL cache by sync strategy bug - Refactor LocationHierarchyHelper pagination - Unit testing - Release 2.2.4

--- plugins/src/main/java/org/smartregister/fhir/gateway/plugins/LocationHierarchyEndpointHelper.java
+++ plugins/src/main/java/org/smartregister/fhir/gateway/plugins/LocationHierarchyEndpointHelper.java
@@ -181,6 +181,7 @@ public class LocationHierarchyEndpointHelper {
             allLocations.add(parentLocation);
         }
         if (childLocationBundle != null) {
+            Utils.fetchAllBundlePagesAndInject(r4FHIRClient, childLocationBundle);
             childLocationBundle.getEntry().parallelStream()
                     .forEach(
                             childLocation -> {
@@ -193,24 +194,6 @@ public class LocationHierarchyEndpointHelper {
                                                 null,
                                                 adminLevels));
                             });
-
-            while (childLocationBundle.getLink(Bundle.LINK_NEXT) != null) {
-                childLocationBundle =
-                        getFhirClientForR4().loadPage().next(childLocationBundle).execute();
-
-                childLocationBundle.getEntry().parallelStream()
-                        .forEach(
-                                childLocation -> {
-                                    Location childLocationEntity =
-                                            (Location) childLocation.getResource();
-                                    allLocations.add(childLocationEntity);
-                                    allLocations.addAll(
-                                            getDescendants(
-                                                    childLocationEntity.getIdElement().getIdPart(),
-                                                    null,
-                                                    adminLevels));
-                                });
-            }
         }
 
         return allLocations;
