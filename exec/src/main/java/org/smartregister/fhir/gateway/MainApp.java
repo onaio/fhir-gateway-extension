8e53fc7ba409ef236e935459ebdc0180a03cdd2c
Martin N <ndegwamartin@users.noreply.github.com>
2026-04-29 10:06:02 +0300

    Refactor dependencies to Spring Boot v3

--- exec/src/main/java/org/smartregister/fhir/gateway/MainApp.java
+++ exec/src/main/java/org/smartregister/fhir/gateway/MainApp.java
@@ -2,14 +2,16 @@ package org.smartregister.fhir.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * This class shows the minimum that is required to create a FHIR Gateway with all AccessChecker
  * plugins defined in "com.google.fhir.gateway.plugin" and "org.smartregister.fhir.gateway.plugins.
  */
 @SpringBootApplication(
-        scanBasePackages = {"org.smartregister.fhir.gateway", "com.google.fhir.gateway.plugin"})
+        scanBasePackages = {"org.smartregister.fhir.gateway", "com.google.fhir.gateway.plugin"},
+        exclude = {DataSourceAutoConfiguration.class})
 @ServletComponentScan({"org.smartregister.fhir.gateway.plugins", "com.google.fhir.gateway"})
 public class MainApp {
 
